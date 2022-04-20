64ad28ca4821179c8802f3c02847451420017888
Family <family@MacBook-Air-Family.local>
2022-04-20 16:53:15 +0300

    vue Todo

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,54 @@
+<template>
+    <form @submit.prevent="onSubmit">
+        <input type="text" v-model="title">
+        <button type="submit">Add New</button>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        onSubmit() {
+            console.log('submit', this.title);
+            if (this.title.trim()) {
+                const newTodo = {
+                    id: Date.now(),
+                    title: this.title,
+                    completed: false
+                }
+
+                this.$emit('add-todo', newTodo)
+                this.title = ''
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    input {
+        margin: 0 auto;
+        width: 50%;
+        height: 30px;
+    }
+
+    button {
+        background: blue;
+        color: #fff;
+        text-transform: uppercase;
+        border-radius: 10%;
+        border: none;
+        font-size: 14px;
+        margin-right: 0;
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h2>Home Page</h2>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, itaque eum. Nemo quasi asperiores excepturi ducimus soluta ipsam dolore eius aliquid commodi, rerum consectetur. Tempora exercitationem quaerat ad consequuntur sunt.</p>
+
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
