0f2355f78e10e418a4f2f925cae70d4ae1f1a872
Family <family@MacBook-Air-Family.local>
2022-04-21 12:36:08 +0300

    error router

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum nesciunt nemo eveniet labore laborum, inventore dolore nobis veniam quaerat soluta, consequuntur obcaecati. Impedit, ut. Velit error ducimus et iure repellat!</p>
+    
+        <a href="/todos">Todos</a>
+    </div>
+</template>
