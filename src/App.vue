6f473b904cc16202b8cf86b740c7dc6e901a269e
Family <family@MacBook-Air-Family.local>
2022-04-21 11:48:41 +0300

    you can add something

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,48 @@
 <template>
   <div id="app">
-    <h1>Todo App</h1>
+    <h2>Todo App</h2>
+    <router-lin to="/">Home</router-lin>
+    <AddTodo 
+        @add-todo="addTodo"
+    />
     <hr>
-
-    
-    <router-view />
+    <TodoList 
+        v-bind:todos="todos"
+        @remove-todo="removeTodo"
+    />
   </div>
 </template>
+
+<script>
+import TodoList from '@/components/TodoList'
+import AddTodo from '@/components/AddTodo'
+export default {
+  name: 'app',
+  data() {
+    return {
+      todos: []
+    }
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(json => {
+        this.todos = json
+      })
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter(t => t.id !== id)
+    },
+    addTodo(todo) {
+      this.todos.push(todo)
+    }
+  },
+  components: {
+    TodoList, AddTodo
+  }
+}
+</script>
 
 <style>
 #app {
